022b8a5336f9d1de4435c3a6b336a20e65ced7e4
rorodaniela <daniela.putri28@gmail.com>
2021-03-17 20:25:07 +0700

    deploy

--- server/services/movies/app.js
+++ server/services/movies/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const {connect} = require('./config/mongodb')
 const app = express()
 const router = require('./routes')
-const port = 4001
+const port = 4001 || process.env.PORT;
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
--- server/services/series/app.js
+++ server/services/series/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const {connect} = require('./config/mongodb')
 const app = express()
 const router = require('./routes/index')
-const port = 4002
+const port = 4002 || process.env.PORT
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
